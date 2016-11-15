b51876d6414724c8a5714b2bb949e2cd45300998
Soichi Tanabe <soichit@uw.edu>
2016-11-15 15:13:12 -0800

    added

--- exercise-4/server.R
+++ exercise-4/server.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 # Read in data
-setwd('~/Documents/info-201/m14-shiny/exercise-4/')
+# setwd("C:/Users/Soichi/Documents/info201/m14-shiny/exercise-4")
 source('./scripts/buildMap.R')
 source('./scripts/buildScatter.R')
 df <- read.csv('./data/electoral_college.csv', stringsAsFactors = FALSE)
@@ -16,12 +16,12 @@ joined.data <- joined.data %>% mutate(ratio = votes/population * 100000)
 
 # Start shinyServer
 shinyServer(function(input, output) { 
-  
   # Render a plotly object that returns your map
   output$map <- renderPlotly({ 
-      return(BuildMap(joined.data, input$mapvar))
+    return(BuildMap(joined.data, input$mapvar))
   }) 
   
-  # Create a `scatter` property on your `output` object. That property shoudl be a `renderPlotly` object that returns a scatterplot (`BuildScatter`)
-  
+  output$scatter <- renderPlotly({
+    return(BuildScatter(joined.data, input$search))
+  })
 })
